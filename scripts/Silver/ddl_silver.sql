fc5dd7402aedd39fb3ae60ee25b324a3cf19d40c
MrIgis <acquahdennis99@gmail.com>
2026-01-07 02:41:44 -0500

    Update ddl_silver.sql

--- scripts/Silver/ddl_silver.sql
+++ scripts/Silver/ddl_silver.sql
@@ -91,3 +91,55 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
 	,dwh_create_date DATETIME2 DEFAULT GETDATE ()
 );
 GO
+--------------------------------------------------------------------------------------------------------------------
+-- Updated DDLs: This update was necessary to enable me to join tables, tables were not able to join because their 
+-- IDs were having different data types
+--------------------------------------------------------------------------------------------------------------------
+IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
+DROP TABLE silver.crm_cust_info;
+GO
+CREATE TABLE silver.crm_cust_info 
+(
+	cst_id NVARCHAR (50),-- Data type was INT but had a challenge joining it with another tables ID which was NVARCHAR.
+	cst_key NVARCHAR (50),
+	cst_firstname NVARCHAR (50),
+	cst_lastname NVARCHAR (50),
+	cst_marital_status NVARCHAR (50),
+	cst_gndr NVARCHAR (50),
+	cst_create_date DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE ()
+);
+
+IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info;
+
+	CREATE TABLE silver.crm_prd_info 
+	(
+		prd_id  NVARCHAR (50), -- Data type was INT but had a challenge joining it with another tables ID which was NVARCHAR.
+		cat_id NVARCHAR (50), -- Add this created column
+		prd_key NVARCHAR (50),
+		prd_nm NVARCHAR (50),
+		prd_cost INT,
+		prd_line NVARCHAR (50),
+		prd_start_dt DATE, -- data type change from DATETIME to DATE
+		prd_end_dt DATE,  -- data type change from DATETIME to DATE
+		dwh_create_date DATETIME2 DEFAULT GETDATE ()
+	);
+
+	IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+
+	CREATE TABLE silver.crm_sales_details (
+		sls_ord_num NVARCHAR(50),
+		sls_prd_key NVARCHAR(50),
+		sls_cust_id  NVARCHAR (50), -- Data type was INT but had a challenge joining it with another tables ID which was NVARCHAR.
+		sls_order_dt DATE,
+		sls_ship_dt DATE,
+		sls_due_dt DATE,
+		sls_sales INT,
+		sls_quantity INT,
+		sls_price INT,
+		dwh_create_date DATETIME2 DEFAULT GETDATE ()
+	);
+-- Reloading silver layer with it updated DDL data types 
+EXEC silver.load_silver
