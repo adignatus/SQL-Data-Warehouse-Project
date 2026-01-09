640178104d2f6d87b61e7541b953f868ee998d74
MrIgis <acquahdennis99@gmail.com>
2026-01-09 04:17:07 -0500

    Update proc_load_silver.sql

--- scripts/Silver/proc_load_silver.sql
+++ scripts/Silver/proc_load_silver.sql
@@ -578,3 +578,56 @@ BEGIN
 	PRINT '======================================================='
 	END CATCH
 END 
+
+
+-- an update on silver.crm_sales_details on the prd_key column... Where I replace the '-' with '_'. 
+
+	SET @start_time = GETDATE()
+		
+	PRINT '>> Truncating Table: silver.crm_sales_details';
+	TRUNCATE TABLE silver.crm_sales_details;
+
+	PRINT '>> Inserting Data Into: silver.crm_sales_details';
+	INSERT INTO silver.crm_sales_details
+	(
+		sls_ord_num,
+		sls_prd_key,
+		sls_cust_id,
+		sls_order_dt,
+		sls_ship_dt,
+		sls_due_dt,
+		sls_sales,
+		sls_quantity,
+		sls_price
+	)
+	SELECT
+		sls_ord_num,
+		REPLACE (sls_prd_key, '-','_'), -- Updated the keys: since it was '-' before and has changed it to '_'
+		sls_cust_id,
+		CASE 
+			WHEN sls_order_dt <0 OR LEN (sls_order_dt) != 8 THEN NULL
+			ELSE CAST (CAST (sls_order_dt AS VARCHAR ) AS DATE)
+		END AS sls_order_dt,
+		CASE 
+			WHEN sls_ship_dt <0 OR LEN (sls_ship_dt) != 8 THEN  NULL
+			ELSE CAST (CAST (sls_ship_dt AS VARCHAR ) AS DATE)
+		END AS sls_ship_dt,
+		CASE 
+			WHEN sls_ship_dt <0 OR LEN (sls_ship_dt) != 8 THEN NULL
+			ELSE CAST (CAST (sls_ship_dt AS VARCHAR ) AS DATE)
+		END AS sls_due_dt,
+		CASE 
+			WHEN sls_sales != sls_quantity * ABS (sls_price) OR sls_sales IS NULL OR sls_sales <= 0
+			THEN sls_quantity * ABS (sls_price)
+			ELSE sls_sales
+		END sls_sales,
+		sls_quantity,
+		CASE 
+			WHEN  sls_price IS NULL OR sls_price <= 0
+			THEN sls_sales / NULLIF (sls_quantity,0)
+			ELSE sls_price
+		END sls_price
+	FROM bronze.crm_sales_details
+
+	SET @end_time = GETDATE()
+		PRINT '>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
